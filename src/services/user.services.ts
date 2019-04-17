0605698de3dc7bbc2629ce48a00852a8a6cf6a14
michaelspeed <michael.the.don@gmail.com>
2019-04-17 17:28:15 +0530

    fixed bcrypt

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -9,10 +9,13 @@ export class UserService {
   ) { }
 
   async adduser(user: User): Promise<User> {
-    bcrypt.hash(user.password, 10).then((hash) => {
+    const newPass = await bcrypt.hash(user.password, 10)
+    user.password = newPass;
+    return this.userRepository.create(user);
+    /*bcrypt.hash(user.password, 10).then((hash) => {
       user.password = hash;
       return this.userRepository.create(user);
-    }).catch(err => console.log(err));
+    }).catch(err => console.log(err));*/
     return null;
   }
   async findAll(): Promise<User[]> {
@@ -21,27 +24,27 @@ export class UserService {
   async findByUsernameAndPassword(user: User): Promise<object> {
     // returning object of type {user: User, error: error}
     return new Promise(async (resolve, reject) => {
-      await bcrypt.hash(user.password, 10).then(hash => {
-        console.log(hash);
-        const findUser = this.userRepository.findOne<User>({
-          where: {
-            username: user.email,
-            password: user.password,
-          },
-        }).then(fuser => {
-          bcrypt.compare(user.password, fuser.password).then(res => {
-            resolve({
-              user: fuser,
-            });
-          }).catch(error => {
-            reject({
-              error,
-            });
-          });
-        }).catch(err => {
+      // this line is not needed
+       /*const hash = await bcrypt.hash(user.password, 10);*/
+       await this.userRepository.findOne<User>({
+        where: {
+          username: user.email,
+        },
+      }).then(async (fuser) => {
+        const valid = await bcrypt.compare(user.password, fuser.password);
+        if (!valid) {
           reject({
-            error: err,
+            error: {message: 'Password does not match'},
           });
+          return;
+        } else {
+          resolve({
+            user: fuser,
+          });
+        }
+      }).catch(err => {
+        reject({
+          error: err,
         });
       });
     });
